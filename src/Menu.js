f7b8f6e1d17ebe8a496d860f0ecbf1ce6be1026e
sara <saraa379@yahoo.com>
2018-10-04 14:53:34 +0200

    started up Menu component

---
+++ src/Menu.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './Menu.css';
+
+class Menu extends Component {
+	constructor(props) {
+			super(props);
+	}
+
+	render() {
+
+      return (
+        <nav>
+            Menu comes here
+        </nav>
+      );//end of return
+	  } //end of render
+} //end of component
+
+export default Menu;
