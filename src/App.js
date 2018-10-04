6837f997ac46beaf9a006c5279b088eabc83d416
Alejandro <92.alejandrogarcia@gmail.com>
2018-10-04 14:06:07 +0200

    implement login with google and basic login component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login'
 import firebase from 'firebase'
-
-const config = {
-  apiKey: "AIzaSyCJVOXUyP9bMysoDBpqN5nDbV9yQPLq3i4",
-  authDomain: "sellyourstuff-b27b2.firebaseapp.com",
-  databaseURL: "https://sellyourstuff-b27b2.firebaseio.com",
-  storageBucket: "sellyourstuff-b27b2.appspot.com",
-};
 
 class App extends Component {
   constructor(props){
@@ -17,26 +11,12 @@ class App extends Component {
 
     }
   }
-  componentDidMount(){
-    firebase.initializeApp(config);
-  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <main className="mainView">
+          <Login/>
+        </main>
       </div>
     );
   }
