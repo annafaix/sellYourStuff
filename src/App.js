0f665818bcee26312f4bb2026749d2450b4efa31
Alejandro <92.alejandrogarcia@gmail.com>
2018-10-08 16:29:05 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     super(props);
     this.state = {
       isLoggedIn: false,
-      currentTab: "products"
+      currentTab: "products",
       currentPage: "login",
       products: []
     }
