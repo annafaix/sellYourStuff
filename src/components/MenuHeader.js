490264f6d010f8b457d9ca0e6b447b649b99a20c
Alejandro <92.alejandrogarcia@gmail.com>
2018-10-18 15:14:05 +0200

    fix logo path

--- src/components/MenuHeader.js
+++ src/components/MenuHeader.js
@@ -30,7 +30,7 @@ export default class MenuHeader extends Component {
       <Segment inverted style={{borderRadius:'0', position:'sticky', width: '100%', top:'0', padding:'0 4rem 0 2rem', zIndex:'5'}}>
         <Menu inverted secondary style={{position:'relative'}}>
           <Menu.Item style={{verticalAlign:"text-top", position:'relative', top:'0', paddingTop:'0'}}>
-            <Image src='/Logo.png' style={{width:'130px', height:'auto'}}/>
+            <Image src='../public/Logo.png' style={{width:'130px', height:'auto'}}/>
           </Menu.Item>
 
           <div id="menuInner" className={this.state.loginStatus ? 'menuInnerShow' : 'hide'}>
