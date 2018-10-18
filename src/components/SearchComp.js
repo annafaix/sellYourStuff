0d0a8e4745f1ce76923ad3be56968516902fb610
Alejandro <92.alejandrogarcia@gmail.com>
2018-10-18 10:50:27 +0200

    fixed bug on componentDidMount on searchComp

--- src/components/SearchComp.js
+++ src/components/SearchComp.js
@@ -7,90 +7,95 @@ import './Search.css';
 export default class SearchComp extends Component {
   constructor(props) {
     super(props);
-    this.state = {value: "", products:[], isLoading: false, results:[]};
+    this.state = { value: "", products: [], isLoading: false, results: [] };
     //this.handleChange = this.handleChange.bind(this);
     this.handleSearchChange = this.handleSearchChange.bind(this);
     this.resetComponent = this.resetComponent.bind(this);
   }
 
-  componentDidMount() {
-      //Retrieving all products from database
-
+  async componentDidMount() {
+    //Retrieving all products from database
+    let editedList = [];
+    let myFetch = () => {
       fetch('http://localhost:3000/api/search/', {
-      method: 'POST',
-      body: "message from search",
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-      }
+        method: 'POST',
+        body: "message from search",
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        }
       })
-      .then(response => {
-        return response.json()
-      }).then(data => {
-        let editedList = [];
+        .then(response => {
+          return response.json()
+        }).then(data => {
 
-        for (var i = 0; i < data.length; i++) {
-          editedList.push({
-          id: data[i]["_id"],
-          title: data[i].name,
-          description: data[i].info,
-          image: data[i].userPicture,
-          price: data[i].price.toString(),
-          category: data[i].category,
-          username: data[i].userName
-        })
-      }
-          this.setState({products: editedList});
+          for (var i = 0; i < data.length; i++) {
+            editedList.push({
+              id: data[i]["_id"],
+              title: data[i].name,
+              description: data[i].info,
+              image: data[i].userPicture,
+              price: data[i].price.toString(),
+              category: data[i].category,
+              username: data[i].userName
+            })
+          }
           //this.setState({products: array});
           //for (var i = 0; i < data.length; i++) {
           //}
-      }).catch(err => {
-        console.log(err);
-      })
-
-      this.resetComponent()
+        }).catch(err => {
+          console.log(err);
+        })
+    }
+    let updateState = editedList => {
+      this.setState({ products: editedList })
+    }
+    await myFetch()
+    await updateState(editedList)
+    this.resetComponent()
 
   }
-//recieves user input in search field
-/*
-  handleChange(event) {
-    this.setState({search: event.target.value});
-  }*/
-  resetComponent(){
+  //recieves user input in search field
+  /*
+    handleChange(event) {
+      this.setState({search: event.target.value});
+    }*/
+  resetComponent() {
     this.setState({ isLoading: false, results: [], value: '' })
   }
-/*
-  generateKey(){
-    this.setState({ key: this.state.key + 1 }, () => {
-      return this.state.key
-    })
-  }*/
+  /*
+    generateKey(){
+      this.setState({ key: this.state.key + 1 }, () => {
+        return this.state.key
+      })
+    }*/
 
   //handleResultSelect = (e, { result }) => this.setState({ value: "" })
 
-    handleResultSelect = (e, { result }) => {
-       this.setState({ value: "" });
+  handleResultSelect = (e, { result }) => {
+    this.setState({ value: "" });
 
-       let tempArray = [];
-       tempArray.push({
-            "_id": {
-                "$oid": result.id},
-            "name": result.title,
-            "price": result.price,
-            "category": result.category,
-            "userName": result.username,
-            "userPicture": result.image,
-            "info": result.description
-       });
+    let tempArray = [];
+    tempArray.push({
+      "_id": {
+        "$oid": result.id
+      },
+      "name": result.title,
+      "price": result.price,
+      "category": result.category,
+      "userName": result.username,
+      "userPicture": result.image,
+      "info": result.description
+    });
 
-       this.props.filterBySearch(tempArray);
-    }
+    this.props.filterBySearch(tempArray);
+  }
 
   handleSearchChange = (e, { value }) => {
     this.setState({ isLoading: true, value })
 
     setTimeout(() => {
       if (this.state.value.length < 1) {
-          this.resetComponent()
+        this.resetComponent()
       }
       const re = new RegExp(_.escapeRegExp(this.state.value), 'i')
       const isMatch = result => re.test(result.title)
@@ -118,23 +123,23 @@ export default class SearchComp extends Component {
 
   render() {
 
-  const { isLoading, value, results } = this.state
-  //console.log("Search value is " + this.state.value);
+    const { isLoading, value, results } = this.state
+    //console.log("Search value is " + this.state.value);
 
     return (
-      <div style={{margin:"3px 20px 3px 20px"}}>
+      <div style={{ margin: "3px 20px 3px 20px" }}>
 
-          <label className="searchLabel">
-              Search
+        <label className="searchLabel">
+          Search
           </label>
-          <Search
-            loading={isLoading}
-            onResultSelect={this.handleResultSelect}
-            onSearchChange={_.debounce(this.handleSearchChange, 30, { loading: true })}
-            results={results}
-            value={value}
+        <Search
+          loading={isLoading}
+          onResultSelect={this.handleResultSelect}
+          onSearchChange={_.debounce(this.handleSearchChange, 30, { loading: true })}
+          results={results}
+          value={value}
 
-          />
+        />
       </div>
     )
   }
