7c40e3401e90babd2464873f83c83900dbbf8d7e
Alejandro <92.alejandrogarcia@gmail.com>
2018-10-18 14:31:48 +0200

    Updates

---
+++ precache-manifest.42ea90e0356e17631281fb9f27735fba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6be49db23c85d514ca711c17dde3b62c",
+    "url": "/sellYourStuff/static/media/image.6be49db2.png"
+  },
+  {
+    "revision": "dad2164f61019c8cd5e01c5a2dd25c73",
+    "url": "/sellYourStuff/static/media/google.dad2164f.ico"
+  },
+  {
+    "revision": "33f25166b9dc116640fb",
+    "url": "/sellYourStuff/static/js/runtime~main.33f25166.js"
+  },
+  {
+    "revision": "0cc10eeebd88d6253b81",
+    "url": "/sellYourStuff/static/js/main.0cc10eee.chunk.js"
+  },
+  {
+    "revision": "40d71119e8499df69696",
+    "url": "/sellYourStuff/static/js/1.40d71119.chunk.js"
+  },
+  {
+    "revision": "0cc10eeebd88d6253b81",
+    "url": "/sellYourStuff/static/css/main.b158fd27.chunk.css"
+  },
+  {
+    "revision": "40d71119e8499df69696",
+    "url": "/sellYourStuff/static/css/1.3a5a82f5.chunk.css"
+  },
+  {
+    "revision": "c015435408a1cf9d08a6f47569170191",
+    "url": "/sellYourStuff/index.html"
+  }
+];
